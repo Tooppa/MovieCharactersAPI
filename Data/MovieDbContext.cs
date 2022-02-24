2094f46500c309281ce77fe4584722e85d4c6c70
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-02-24 13:40:16 +0200

    CHORE: added connection string and entityframwork.tools

---
+++ Data/MovieDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MovieCharactersAPI.Models;
+
+namespace MovieCharactersAPI.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public DbSet<Character> Characters { get; set; }
+        /// <summary>
+        /// Gets the string required for the connection to the database
+        /// </summary>
+        /// <returns>
+        /// String from SqlConnectionStringBuilder with right information
+        /// </returns>
+        private static string GetConnectionString()
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+            // search local machine for SQL Express Server instance
+            sqlConnectionStringBuilder.DataSource = ".\\SQLEXPRESS";
+            sqlConnectionStringBuilder.InitialCatalog = "MovieCharacters";
+            sqlConnectionStringBuilder.IntegratedSecurity = true;
+            // otherwise connection fails due to untrusted SSL certificate chain
+            sqlConnectionStringBuilder.TrustServerCertificate = true;
+            return sqlConnectionStringBuilder.ConnectionString;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(GetConnectionString());
+        }
+    }
+}
