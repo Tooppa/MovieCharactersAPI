c22d8cfb8723df88956a56abdcf16942e014286e
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-02-24 13:24:44 +0200

    CHORE: added character class and entityframeworkcore.sqlserver form nuget

---
+++ Models/Character.cs
@@ -0,0 +1,17 @@
+﻿namespace MovieCharactersAPI.Models
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Alias { get; set; }
+        public Gender Gender { get; set; }
+        public string Picture { get; set; }
+    }
+}
