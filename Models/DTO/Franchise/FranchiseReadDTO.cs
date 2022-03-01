2a8734be8955fc8b8f0249e5e256a07af349567d
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2022-03-01 08:53:13 +0200

    FEAT: Add franchise DTOs

--- Models/DTO/Franchise/FranchiseReadDTO.cs
+++ Models/DTO/Franchise/FranchiseReadDTO.cs
@@ -1,6 +1,13 @@
-﻿namespace MovieCharactersAPI.Models.DTO.Franchise
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieCharactersAPI.Models.DTO.Franchise
 {
     public class FranchiseReadDTO
     {
+        public int Id { get; set; }
+        [MaxLength(50, ErrorMessage = "Name can't be longer than 50 characters.")]
+        public string Name { get; set; }
+        [MaxLength(100, ErrorMessage = "Description can't be longer than 100 characters.")]
+        public string Description { get; set; }
     }
 }
